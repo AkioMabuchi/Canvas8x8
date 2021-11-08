319b9feffa3e1a75ce0242400681f0e6f5be1e32
AkioMabuchi <mabuchiakio@gmail.com>
2021-11-09 08:04:38 +0900

    fixed bug

--- Assets/Scripts/Canvases/CanvasPallet.cs
+++ Assets/Scripts/Canvases/CanvasPallet.cs
@@ -8,21 +8,6 @@ namespace Canvases
     {
         [SerializeField] private Image imageFrame;
 
-        [SerializeField] private Transform transformColors;
-        [SerializeField] private GameObject prefab;
-
-        private void Start()
-        {
-            for (int i = 0; i < 20; i++)
-            {
-                GameObject gameObjectColor = Instantiate(prefab, transformColors);
-                gameObjectColor.name = "ImageColor (" + i + ")";
-                float positionX = i % 10 * 60.0f;
-                float positionY = i / 10 * 60.0f;
-                gameObjectColor.transform.localPosition = new Vector3(positionX, positionY, 0.0f);
-            }
-        }
-
         public void Show()
         {
             imageFrame.gameObject.SetActive(true);
